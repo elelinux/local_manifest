df6ac954104c5c7b91c6ca7a4420c2b67e35d1bb
Brandon McAnsh <brandon.mcansh@gmail.com>
2013-12-23 10:22:22 -0500

    Update picks.sh

--- picks.sh
+++ picks.sh
@@ -12,8 +12,8 @@ function pick() {
 }
 
 declare -a repos=("device_htc_m7-common" "frameworks_base")
-declare -a repo_arrays=('m7-common' 'fw_base' 'settings')
-declare -a m7-common=('14130' '14091')
+declare -a repo_arrays=('m7_common' 'fw_base' 'settings')
+declare -a m7_common=('14130' '14091')
 declare -a fw_base=('14068' '14069')
 
 for index in ${!repos[*]}; do
