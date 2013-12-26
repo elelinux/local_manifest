c20194c8cce7473ccdb12708dd1a548f8c26c5a8
Brandon McAnsh <brandon.mcansh@gmail.com>
2013-12-26 10:54:06 -0500

    Update picks.sh

--- picks.sh
+++ picks.sh
@@ -14,7 +14,7 @@ function pick() {
 
 #declare -a repos=("device_htc_m7-common" "frameworks_base")
 #declare -a repo_arrays=('m7_common' 'fw_base' 'settings')
-declare -a m7_common=('14091' '14345')
+declare -a m7_common=('14345')
 declare -a htc_msm8960=('14346')
 declare -a fw_base=('14068' '14221')
 
