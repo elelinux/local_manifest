e7260867ebfaa25941a08e740f498e9cb9b16838
Brandon McAnsh <brandon.mcansh@gmail.com>
2014-01-20 22:13:17 -0500

    all things verified and merged

--- picks.sh
+++ picks.sh
@@ -26,43 +26,8 @@ function pick() {
    done
 }
 
-declare -a bionic=('15386' '15387' '15388' '15389' '15390' '15391' '15392' '15393' '15394' '15395' '15396')
 declare -a build=('14560')
-declare -a dalvik=('15397')
-declare -a m7_common=('15398' '15399' '15400')
-declare -a fw_base=('15403' '15404' '15405' '15406' '15407' '15408' '15409' '15410' '15411' '15412' '15413')
-declare -a fw_rs=('15401')
-declare -a htc=('15402')
-# declare -a romcontrol=('')
-
-cd bionic
-pick bionic[@]
-cd -
 
 cd build
 pick build[@]
 cd -
-
-cd dalvik
-pick dalvik[@]
-cd -
-
-cd device/htc/m7-common
-pick m7_common[@]
-cd -
-
-cd frameworks/base
-pick fw_base[@]
-cd -
-
-cd frameworks/rs
-pick fw_rs[@]
-cd -
-
-cd vendor/htc
-pick htc[@]
-cd -
-
-# cd packages/apps/ROMControl
-# pick romcontrol[@]
-# cd -
