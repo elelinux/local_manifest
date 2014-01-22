9e477610c2dc3941facf5ff4ad2c6a58e0fdf0e0
Brandon McAnsh <brandon.mcansh@gmail.com>
2014-01-22 14:35:42 -0500

    new picks

--- picks.sh
+++ picks.sh
@@ -7,6 +7,10 @@
 
 . helper_functions
 
+function cr() {
+   cd - > /dev/null
+}
+
 if [ "$1" == "--verify" ]; then
    verify=1
    [ -f $ROOT/.files_to_verify ] && rm $ROOT/.files_to_verify
@@ -26,8 +30,33 @@ function pick() {
    done
 }
 
-declare -a build=('14560')
+declare -a build=('14560' '15458')
+declare -a fw_base=('14470' '15062' '15432' '15374' '15434' '15450' '15459')
+declare -a camera2=('15379')
+declare -a gallery2=('15378')
+declare -a romcontrol=('14555' '15435' '15330')
+declare -a telephony=('14562')
 
 cd build
 pick build[@]
-cd -
+cr
+
+cd frameworks/base
+pick fw_base[@]
+cr
+
+cd packages/apps/Camera2
+pick camera2[@]
+cr
+
+cd packages/apps/Gallery2
+pick gallery2[@]
+cr
+
+cd packages/apps/ROMControl
+pick romcontrol[@]
+cr
+
+cd packages/services/Telephony
+pick telephony[@]
+cr
